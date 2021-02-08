5702a4a662784cb38a1e9e7a9c720fa62e154ab4
Josh Benson <josh.benson.805@gmail.com>
2021-02-07 23:19:07 -0800

    p

--- main.py
+++ main.py
@@ -10,14 +10,7 @@ from instagram_private_api import (
     __version__ as client_version)
 
 username = "testaccforapi"
-api = Client("testaccforapi", password, auto_patch=True)
-# rank_token = api.generate_uuid(return_hex=False, seed=None)
-# rank_token = '2abc9200-76e4-11ea-ab20-001a7dda7113'
-# user_id1 = "32341377860"  # testaccforapi
-# user_id2 = "5565476890"  # ella
-# user_id3 = "243946204"  # josh
-# user_id4 = "32649564590"  # pratik
-# arr = [user_id1, user_id2, user_id3]
+api = Client(username, password, auto_patch=True)
 
 
 if __name__ == '__main__':
